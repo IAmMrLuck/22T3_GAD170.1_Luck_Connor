2a846c3c9c9fc99e1083bd6f82121cb4d69e0d41
IAmMrLuck <114543523+IAmMrLuck@users.noreply.github.com>
2022-10-07 00:50:24 +1100

    Finished Project

--- Assets/Scripts by (Connor Luck)/LevellingSystem.cs
+++ Assets/Scripts by (Connor Luck)/LevellingSystem.cs
@@ -15,26 +15,39 @@ namespace ConnorLuck
         [SerializeField] private float currentXp;
         [SerializeField] private float currentLevel;
         [SerializeField] private float levelThreshhold;
+        // these are my variables
 
         private void Start()
         {
             currentLevel = 1f;
             currentXp = 0f;
             levelThreshhold = currentLevel * 100;
+            // these are the values i have stored within my variables
+
             Debug.Log("My current level is " + currentLevel + ".");
             Debug.Log("I have " + currentXp + "XP.");
             Debug.Log(levelThreshhold + " 'till next level.");
-            currentXp = Random.Range(90, 118);
+            // the debug function allows what i write next to be displayed in the console
+            // it's a great way to check that what i've added is working
+
+            currentXp = Random.Range(50, 100);
+            // this generates a random number, between 50 and 100
+
             Debug.Log("I have " + currentXp + " XP.");
+            // more debug to be displayed in the console
             
             if (currentLevel > levelThreshhold)
             {
                 currentLevel++;
                 Debug.Log("" + currentLevel);
             }
-            
+            // this will increase the player level by 1 when they have met the level threshold
+
             Debug.Log("My current level is " + currentLevel + ".");
             Debug.Log(levelThreshhold + " 'till next level."); 
+            // even more debugging
+
+
             // TODO XP Final: Add code comments describing what you hope your code is doing throughout this script.
 
             // TODO XP Bonus: Adjust our character's stats ("runSpeed" and/or "jumpStrength") based on their level. (Hint: You'll need a reference to the SimpleCharacterController script!)
--- Assets/Scripts by (Connor Luck)/SimpleCharacterController.cs
+++ Assets/Scripts by (Connor Luck)/SimpleCharacterController.cs
@@ -12,34 +12,66 @@ namespace Connor
     {
         [SerializeField] private float horizontalInputValue; // The value of our horizontal input axis.
         [SerializeField] private SpriteRenderer spriteRenderer; // Our character's sprite.
+        [SerializeField] private BoxCollider2D boxCollider;
+        [SerializeField] private LayerMask groundLayer;
+        [SerializeField] private Rigidbody2D rbody2D; 
+        // i've created references to call back to
 
-        [SerializeField] private Rigidbody2D rbody2D;
-        [SerializeField] private float runSpeed = 3f;
-        [SerializeField] private float jumpStrength = 5f;
-
-        private float xInput = 0f;
-        private bool isJumping = false;
+        [SerializeField] private float runSpeed = 7f; 
+        // this and the next, are the starting values of my stats - I'm holding values for running and jumping
+        [SerializeField] private float jumpStrength = 3f;
 
         private void Start()
         {
             rbody2D = GetComponent<Rigidbody2D>();
+            boxCollider = GetComponent<BoxCollider2D>();
+            // this checks for the component rigidbody2D, and store it in the rbody2D variable
         }
 
         private void Update()
         {
-            xInput = Input.GetAxisRaw("Horizontal");
-            isJumping = Input.GetKeyDown(KeyCode.Space);
 
-            transform.position += new Vector3(xInput * runSpeed, 0, 0) * Time.deltaTime;
+            float horizontalInput = (Input.GetAxis("Horizontal"));
 
-            if (isJumping)
-            {
-                rbody2D.velocity = Vector2.up * jumpStrength;
-            }
+            rbody2D.velocity = new Vector2(horizontalInput * runSpeed, rbody2D.velocity.y);
+            // the value i've used is defined by Unity - and it checks if the directoinal keys (or A,D) are pressed and changes the Horizontal Axis by +1 or -1
+            // this however, leaves us with very slow movemet - so that is why i have declared my speed variable above and used "*runspeed"
+
+            if (horizontalInput > 0.01f)
+                transform.localScale = Vector3.one;
+            else if (horizontalInput < -0.01f)
+                transform.localScale = new Vector3(-1, 1, 1);
+            // this flips my guy when i run left to right. this can be represented in the inspector using the Scale. 1, he is facing left, and -1 he is facing right
+            // i admittedly don't fully understand the (-1, 1, 1) but i know that it works.
+            // i also needed to flip this dude in unity so that it would line up correctly with the buttons
+            // i also added an animation for his idle state - i didn't think there was enough to add a run animation
+
+
+            if (Input.GetKeyDown(KeyCode.Space))
+                Jump();
+
+            if (Input.GetKeyUp(KeyCode.Space) && rbody2D.velocity.y > 0)
+                rbody2D.velocity = new Vector2(rbody2D.velocity.x, rbody2D.velocity.y / 2);
+
+            //this method here prevents the player from holding dowm jump to fly off the screen
+            // but it doesn't prevent players from rapidly tapping the button to continue jumping
+
         }
-        
-            // TODO Movement Bonus 2: Flip our character's sprite so that it faces left/right if we are moving left/right. (Hint: A SpriteRenderer reference, and changing its FlipX = true/false will help!)
 
-        
+        private void Jump()
+        {
+            rbody2D.velocity = new Vector3(rbody2D.velocity.x, runSpeed, jumpStrength);
+
+        }
+        // i've added this to optimise my jump method
+        // i've also added "runSpeed" here to ensure that my character doesnm't slow down when jumping
+
+
+        private bool onGround()
+        {
+            RaycastHit2D raycastHit = Physics2D.BoxCast(boxCollider.bounds.center, boxCollider.bounds.size, 0, Vector2.down, 0.1f, groundLayer);
+            return raycastHit.collider != null;
+            // i found that this was the way to it, but i didn't understand this 
+        }
     }
 }
